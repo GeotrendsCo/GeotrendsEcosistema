fd0e6ab0ec159ef1efe21ace9aaa279f3ac3e71e
Jonathan Ochoa <macos@MacBook-Pro-de-Jonathan-Ochoa.local>
2024-09-04 20:05:06 -0500

    cambio de servidor

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000', // Redirige las solicitudes de API al servidor Express
+      '/api': 'http:44.213.153.17:3000', // Redirige las solicitudes de API al servidor Express
     },
   },
 });
